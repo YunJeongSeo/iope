053ba325befbf9087aaf8f5a12d21ad74274d904
yun <155128053+YunJeongSeo@users.noreply.github.com>
2024-01-12 14:57:35 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  // $('.news_wrap .container .con6').
+
+  // $('.news_wrap .blind_box').hover(function(){
+  //   $('.news_wrap .news_img').css({scale : 1.2})
+  //   // $('news_img').scale(2)  
+  // }, function(){
+  //   $('.news_wrap .news_img').css({scale : 1})
+  //   // $('news_img').scale(1)  
+  // })
+  
+  
+  
+  // $('.skin_wrap .blind_box').hover(function(){
+  //   $('.skin_img').css({scale : 1.2})
+  // }, function(){
+  //   $('.skin_img').css({scale : 1})
+  // })
+
+
+
+  // function react(){
+  //   if($(window).width() <= 450){
+  //     $('.sec05 .sec_inner').addClass('swiper');
+  //   }
+  // };
+
+  // react();
+
+
+
+
+
+
+
+
+
+});//end
